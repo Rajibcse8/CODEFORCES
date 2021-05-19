353da156b2183d7601efb2083dcae47309855c65
Rajibcse8 <73878397+Rajibcse8@users.noreply.github.com>
2021-05-19 23:39:31 +0600

    Create 1325A - EhAb AnD gCd.java

---
+++ 1325A - EhAb AnD gCd.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Code {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t--!=0) {
+			int x=sc.nextInt();
+			System.out.println("1 "+(x-1));
+		}
+	}
+}
